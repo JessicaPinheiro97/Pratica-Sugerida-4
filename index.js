9d3151311c0c6301087663b149c52e4b0a5fb07b
Jéssica Pinheiro <109962238+JessicaPinheiro97@users.noreply.github.com>
2022-12-17 03:28:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var idade = 20;
+var altura = 1.75;
+var nome = "José";
+var pagou = true;
+
+console.log("Os valores atribuídos as variáveis são: ");
+console.log("Idade: ", idade);
+console.log("Altura: ", altura);
+console.log("Nome: ", nome);
+console.log("Pagou: ", pagou);
+
+console.log("Os tipos das variáveis são: ");
+console.log("idade ", typeof (idade));
+console.log("altura ", typeof (altura));
+console.log("nome ", typeof (nome));
+console.log("pagou ", typeof (pagou));
+
+//Práticas sobre os tipos de variáveis;
